d88f233210a206ce88dd8c1d2173632e54bed92d
Степан <elkinstepan405@gmail.com>
2023-10-28 19:02:08 +0500

    Commit one

---
+++ matPlot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+
+x = np.arange(0,4*np.pi,0.1)   # start,stop,step
+y = np.sin(x) * np.cos(x)
+plt.plot(x, y)
+plt.show()
